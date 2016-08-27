899a168a06bae933a284eafe356ff1d4b75e885b
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-08-27 23:12:46 +0800

    Install redux , and update reducers indexj.js

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -1,4 +1,4 @@
-import { combineReducers } from 'react-redux';
+import { combineReducers } from 'redux';
 import rolls from './RollReducer';
 import players from './PlayerReducer';
 import games from './GameReducer';
